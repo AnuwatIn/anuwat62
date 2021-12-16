9e4aeea53d96902056598a20cda5b8149732def8
AnuwatIn <95852653+AnuwatIn@users.noreply.github.com>
2021-12-16 11:50:24 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ var fs = require('fs');
 var http = require('http');
 var https = require('https');
 //var privateKey  = fs.readFileSync(path.resolve('server/key.pem', 'utf8'));
-var privateKey  = fs.readFileSync('server.key', 'utf8');
-var certificate = fs.readFileSync('server.crt', 'utf8');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/lookago.ddns.net/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/lookago.ddns.net/fullchain.pem', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 const app = express();
 
